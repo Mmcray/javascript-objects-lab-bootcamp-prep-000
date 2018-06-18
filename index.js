6ca4e59a71fe0c6bdd3907fecc078ef496877925
Michael McRay <mmcray5@gmail.com>
2018-06-18 22:36:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,4 +7,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value;
   return object;
 }
-
+function updateObjectWithKeyAndValue (object, key, value) {
+  return Object.assign({}, object, { [key]: value });
+}
